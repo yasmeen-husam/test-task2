0a48a1549911b651e754cb0372382538dd623ea5
Rasha Alkhawaldeh <alkhawaldehrasha7@gmail.com>
2025-02-16 05:51:50 -0900

    Add files via upload

---
+++ GlobalSection(ProjectConfigurationPlatfo.cs
@@ -0,0 +1,14 @@
+GlobalSection(ProjectConfigurationPlatforms) = postSolution  
+	{EFD50C5D-6B7C-45A2-AD89-D87A138ACCF1}.Debug|Any CPU.ActiveCfg = Debug|Any CPU  
+	{EFD50C5D-6B7C-45A2-AD89-D87A138ACCF1}.Debug|Any CPU.Build.0 = Debug|Any CPU  
+	{EFD50C5D-6B7C-45A2-AD89-D87A138ACCF1}.Release|Any CPU.ActiveCfg = Release|Any CPU  
+	{EFD50C5D-6B7C-45A2-AD89-D87A138ACCF1}.Release|Any CPU.Build.0 = Release|Any CPU  
+EndGlobalSection  
+
+GlobalSection(SolutionProperties) = preSolution  
+	HideSolutionNode = FALSE  
+EndGlobalSection  
+
+GlobalSection(ExtensibilityGlobals) = postSolution  
+	SolutionGuid = {BF1EDE33-C08E-4B00-BDB0-8A62303B2C02}  
+EndGlobalSection  
